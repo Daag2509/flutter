33c8936ce247fb84834aed316923d9e9a3a4a1f5
Daag2509 <daniel25aponte@gmail.com>
2025-07-23 20:14:42 -0400

    Delete lib directory

--- lib/patch.dart
+++
@@ -1,9 +0,0 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-Future<void> updateTodoCompletion(int id, bool completed) async {
-  await Supabase.instance.client
-      .from('todos')
-      .update({'completed': completed})
-      .eq('id', id)
-      .execute();
-}
